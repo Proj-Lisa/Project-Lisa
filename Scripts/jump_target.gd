2ca3f2b5be24c972845cd354b371d71af4628ecd
GeoKuhns <85414645+GeoKuhns@users.noreply.github.com>
2025-07-01 03:02:45 -0400

    Move Refine + Orbs
    Sweetspot soon into Long and High Jumps, instead of at very end. Vault v2. Replaced magic accelerators with jump boosts. Micro-puzzles added with orbs rewarding jumps, showing slides, and highlighting movement thru the level.

--- Scripts/jump_target.gd
+++ Scripts/jump_target.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-@onready var vaulter = %Mona
+@onready var leaper = %Mona
 @onready var pathFollow = $Path2D/PathFollow2D
 
-var canVault = false
-var isVaulting = false
+var canLeap = false
+var isLeaping = false
 var isFacingLeft = false
 var pathSpeed = 0.15
 
@@ -16,34 +16,34 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	#activates vault
-	if canVault and Input.is_action_pressed("dash"):
-		print("Vaulted!")
-		isVaulting = true
+	if canLeap and Input.is_action_pressed("jump"):
+		print("Leaped!")
+		isLeaping = true
 		
 	#stops the vault
 	if pathFollow.progress_ratio >= 1:
-		isVaulting = false
+		isLeaping = false
 		pathFollow.progress_ratio = 0
 	
 	#applies the path, based on the pixels of the path
-	if isVaulting:
+	if isLeaping:
 		pathFollow.progress_ratio += pathSpeed
-		vaulter.velocity.y += pathFollow.progress * -1 #to go up
-		if isFacingLeft:
-			vaulter.velocity.x += pathFollow.progress * -1
-		else:
-			vaulter.velocity.x += pathFollow.progress
-		vaulter.monaSprite.play("H-Vault")
+		leaper.velocity.y += pathFollow.progress * -1 #to go up
+		
+		#no X direction right now. change to apply vectors for rotation purposes?
+		#if isFacingLeft:
+		#	leaper.velocity.x += pathFollow.progress * -1
+		#else:
+		#	leaper.velocity.x += pathFollow.progress
+		leaper.monaSprite.play("H-Vault")
 
 func _on_body_entered(body: Node2D) -> void: #upon entering vault area, can follow path
-	#print("vault area")
-	
 	if "Mona" in body.name:
-		vaulter = body
-		if vaulter.velocity.y < 0: #can only vault if goingUp
-			canVault = true
+		leaper = body
+		if leaper.velocity.y <= 0: #cannot leap if going down
+			canLeap = true
 
 func _on_body_exited(body: Node2D) -> void:
 	if "Mona" in body.name:
-		canVault = false
-	isVaulting = false			#upon leaving area, no more affect on Player
+		canLeap = false
+	isLeaping = false			#upon leaving area, no more affect on Player
